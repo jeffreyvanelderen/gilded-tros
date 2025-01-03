1e96bc9fd64ee1fe28ed85c139e57955d1e4bfe0
jeffreyvanelderen <jeffrey.vanelderen@acagroup.be>
2025-01-03 13:34:48 +0100

    GILTRO-1 Refactor string duplication to reusable ItemType.ts enum and update references - Update for let i loop to for item in loop - No changes to business logic yet - Add TODO

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -1,4 +1,5 @@
 import {Item} from './item';
+import {ItemType} from "./models/enums/ItemType";
 
 export class GildedTros {
 
@@ -7,52 +8,53 @@ export class GildedTros {
     }
 
     public updateQuality(): void {
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Good Wine' && this.items[i].name != 'Backstage passes for Re:Factor'
-                && this.items[i].name != 'Backstage passes for HAXX') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'B-DAWG Keychain') {
-                        this.items[i].quality = this.items[i].quality - 1;
+        for (const currentItem of this.items) {
+            if (currentItem.name != ItemType.GOOD_WINE && currentItem.name != ItemType.BACKSTAGE_PASSES_REFACTOR
+                && currentItem.name != ItemType.BACKSTAGE_PASSES_HAXX) {
+                if (currentItem.quality > 0) {
+                    if (currentItem.name != ItemType.B_DAWG_KEYCHAIN) {
+                        currentItem.quality = currentItem.quality - 1;
                     }
                 }
             } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1;
+                if (currentItem.quality < 50) {
+                    currentItem.quality = currentItem.quality + 1;
 
-                    if (this.items[i].name == 'Backstage passes for Re:Factor') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1;
+                    if (currentItem.name == ItemType.BACKSTAGE_PASSES_REFACTOR) {
+                        if (currentItem.sellIn < 11) {
+                            if (currentItem.quality < 50) {
+                                currentItem.quality = currentItem.quality + 1;
                             }
                         }
 
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1;
+                        if (currentItem.sellIn < 6) {
+                            if (currentItem.quality < 50) {
+                                currentItem.quality = currentItem.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (this.items[i].name != 'B-DAWG Keychain') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
+            if (currentItem.name != ItemType.B_DAWG_KEYCHAIN) {
+                currentItem.sellIn = currentItem.sellIn - 1;
             }
 
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Good Wine') {
-                    if (this.items[i].name != 'Backstage passes for Re:Factor' || this.items[i].name != 'Backstage passes for HAXX') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'B-DAWG Keychain') {
-                                this.items[i].quality = this.items[i].quality - 1;
+            if (currentItem.sellIn < 0) {
+                if (currentItem.name != ItemType.GOOD_WINE) {
+                    // @ts-ignore
+                    if (currentItem.name != ItemType.BACKSTAGE_PASSES_REFACTOR || currentItem.name != ItemType.BACKSTAGE_PASSES_HAXX) {
+                        if (currentItem.quality > 0) {
+                            if (currentItem.name != ItemType.B_DAWG_KEYCHAIN) {
+                                currentItem.quality = currentItem.quality - 1;
                             }
                         }
                     } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality;
+                        currentItem.quality = currentItem.quality - currentItem.quality;
                     }
                 } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1;
+                    if (currentItem.quality < 50) {
+                        currentItem.quality = currentItem.quality + 1;
                     }
                 }
             }
---
+++ TypeScript/src/models/enums/ItemType.ts
@@ -0,0 +1,7 @@
+
+export enum ItemType {
+    GOOD_WINE = "Good Wine",
+    BACKSTAGE_PASSES_REFACTOR = "Backstage passes for Re:Factor",
+    BACKSTAGE_PASSES_HAXX = "Backstage passes for HAXX",
+    B_DAWG_KEYCHAIN = "B-DAWG Keychain"
+}
